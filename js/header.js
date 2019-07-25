5b549d080c4b8a4f910b3b4411fa98b6cbe546fe
Elise Minto <Elise.Minto@target.com>
2019-07-25 14:58:56 -0500

    header added

--- js/header.js
+++ js/header.js
@@ -1,5 +1,19 @@
 document.write(`
-    <div class="header" style="margin: 20px;">
-        <p></p>
+    <div class="header">
+        <span id="toggleMenu" class="" onclick="openNav()">
+            <button class="menuButton" type="button" aria-label="Menu" aria-controls="navigation">
+                <div class="hamburgerButton"></div>
+                <div class="hamburgerButton"></div>
+                <div class="hamburgerButton"></div>
+            </button>
+        </span>
+        <span id="title">
+            Elise Minto
+        </span>
+        <span id="social">
+            <a href="#" class="fa fa-linkedin"></a>
+            <a href="#" class="fa fa-instagram"></a>
+            <a href="#" class="fa fa-facebook"></a>
+        </span>
     </div>
 `)
--- js/sidenav.js
+++ js/sidenav.js
@@ -1,17 +1,10 @@
 document.write(`
     <aside id="mySidenav" class="sidenav">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-        <a href="../html/about.html">about me</a>
-        <a href="../html/resume.html">resume</a>
-        <a href="../html/artwork.html">portfolio</a>
+        <div style="margin-top: 50px;">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <a href="../html/about.html">about me</a>
+            <a href="../html/resume.html">resume</a>
+            <a href="../html/artwork.html">portfolio</a>
+        </div>
     </aside>
-
-
-    <span id="toggleMenu" class="" onclick="openNav()">
-        <button class="menuButton" type="button" aria-label="Menu" aria-controls="navigation">
-            <div class="hamburgerButton"></div>
-            <div class="hamburgerButton"></div>
-            <div class="hamburgerButton"></div>
-        </button>
-    </span>
 `)
