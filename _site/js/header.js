94daed0e8404b968f6d236381890477ec8aac9cf
eminto <eminto@umich.edu>
2019-09-12 16:10:49 -0400

    build w jekyll

---
+++ _site/js/header.js
@@ -0,0 +1,19 @@
+document.write(`
+    <div class="header">
+        <span id="toggleMenu" class="" onclick="openNav()">
+            <button class="menuButton" type="button" aria-label="Menu" aria-controls="navigation">
+                <div class="hamburgerButton"></div>
+                <div class="hamburgerButton"></div>
+                <div class="hamburgerButton"></div>
+            </button>
+        </span>
+        <span id="title">
+            Elise Minto
+        </span>
+        <span id="social">
+            <a href="https://www.linkedin.com/in/elise-minto/" class="fa fa-linkedin" target="_blank"></a>
+            <a href="https://www.instagram.com/e.minto22/" class="fa fa-instagram" target="_blank"></a>
+            <a href="https://www.facebook.com/elise.minto" class="fa fa-facebook" target="_blank"></a>
+        </span>
+    </div>
+`)
---
+++ _site/js/script.js
@@ -0,0 +1,10 @@
+function openNav(){
+    document.getElementById("mySidenav").style.width = "200px";
+    document.getElementById("resume").style.marginLeft = "300px";
+    
+}
+
+function closeNav(){
+    document.getElementById("mySidenav").style.width = "0px";
+    document.getElementById("resume").style.marginLeft = "200px";
+}
